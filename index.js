d4ffc2a8a07c123306f323440e0d957346a3108f
omkar <omkarshevare7@gmail.com>
2022-03-28 11:25:37 +0530

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const options = {
   cert: fs.readFileSync(`./cert.pem`)
 };
 
-const port = 4000;
+const port = process.env.port || 4000;
 
 https.createServer(options, (req, res) => {
   res.writeHead(200);
